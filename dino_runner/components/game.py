235427fc8530a071b0e84ea1039b0d30d4a9cc43
Erik <erikbarros1402@gmail.com>
2022-12-12 22:03:15 -0300

    aula #1

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,6 +1,6 @@
 import pygame
 
-from dino_runner.components.dino import Dinosaur
+from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 
 
