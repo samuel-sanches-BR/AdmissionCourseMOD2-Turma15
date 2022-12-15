fa84d040059dec84311031ae2b3ca0daff9d7611
Samuel Sanches <samuelsanches@gmail.com>
2022-12-14 21:38:58 -0300

    Classe 3 Score e Menu

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -18,9 +18,14 @@ class ObstacleManager:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
+                
                 game.playing = False
+                game.death_count += 1
                 break
 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+    def reset_obstacles(self):
+        self.obstacles = []
